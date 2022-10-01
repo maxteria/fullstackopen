8a84b0e191beba7d516a89328dbb429fd68fdffc
Maximiliano Morales <maxi69@gmail.com>
2022-10-01 18:23:37 -0300

    Adding PART1 | Exercise 1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 const Header = (props) => {
   return (
     <header>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </header>
   );
 };
@@ -13,50 +13,52 @@ const Content = (props) => {
   return (
     <>
       <p>
-        {props.parts[0].name} {props.parts[0].exercises}
+        {props.course.parts[0].name} {props.course.parts[0].exercises}
       </p>
       <p>
-        {props.parts[1].name} {props.parts[1].exercises}
+        {props.course.parts[1].name} {props.course.parts[1].exercises}
       </p>
       <p>
-        {props.parts[2].name} {props.parts[2].exercises}
+        {props.course.parts[2].name} {props.course.parts[2].exercises}
       </p>
     </>
   );
 };
 
 const Total = (props) => {
-  let  _total = 0;
-  
-  props.parts.forEach(element => {
+  let _total = 0;
+
+  props.course.parts.forEach((element) => {
     _total += element.exercises;
   });
-  
+
   return <p>Number of exercises {_total}</p>;
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </>
   );
 };
