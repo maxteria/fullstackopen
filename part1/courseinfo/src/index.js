d2903ca3de150df9c23fadbf8e1cab3162ec2801
Maximiliano Morales <maxi69@gmail.com>
2022-10-01 17:43:31 -0300

    Adding PART1 - Exercise 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 const Header = (props) => {
   return (
     <header>
@@ -9,7 +8,6 @@ const Header = (props) => {
     </header>
   );
 };
-
 
 const Part = (props) => {
   return (
@@ -19,30 +17,33 @@ const Part = (props) => {
   );
 };
 
-
 const Total = (props) => {
-  return (
-    <p>Number of exercises {props.total}</p>
-  )
-}
-
+  return <p>Number of exercises {props.total}</p>;
+};
 
 const App = () => {
+
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <>
       <Header course={course} />
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </>
   );
 };
