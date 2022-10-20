51b86ef66a4cc1a8a08cf87962a1486c7fd33880
Maximiliano Morales <maxteriaok@gmail.com>
2022-10-20 13:45:05 -0300

    Feature: Conditions to display countries

--- part2/countriesinfo/src/App.js
+++ part2/countriesinfo/src/App.js
@@ -1,9 +1,34 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function App() {
+const Country = ({ country }) => {
+  console.log(country)
+  return <>
+  <h1>{country.name}</h1>
+  </>;
+};
+
+const Countries = ({ countries }) => {
+  if (countries.length === 0) return <p>No results for term.</p>;
+  if (countries.length === 1) return <Country country={countries} />;
+  if (countries.length > 10)
+    return <p>Too many matches, specify another filter</p>;
+
+  return (
+    <>
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name}>{country.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const App = () => {
   const [countries, setCountries] = useState([]);
-  const [newCountry, setNewCountry] = useState("");
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [country, setCountry] = useState("");
 
   const hook = () => {
     axios.get("https://restcountries.com/v2/all").then((response) => {
@@ -14,27 +39,21 @@ function App() {
   useEffect(hook, []);
 
   const handleChangeCountry = (e) => {
-    setNewCountry(e.target.value);
+    const countryTerm = e.target.value.toLowerCase();
+    setCountry(countryTerm);
+    setFilteredCountries(() =>
+      countries.filter(
+        (country) => country.name.toLowerCase().search(countryTerm) >= 0
+      )
+    );
   };
-
-  const filteredCountries = () =>
-    countries.filter((country) => {
-      console.log(country.name);
-      let _country = country.name.toLowerCase();
-      let _filter = newCountry.toLowerCase();
-
-      return _country.search(_filter) >= 0;
-    });
 
   return (
     <>
-      <h1>Countries Info</h1>
-      <input onChange={handleChangeCountry} />
-      {filteredCountries().map((country) => (
-        <li key={country.name}>{country.name}</li>
-      ))}
+      <input onChange={handleChangeCountry} value={country} />
+      {country && <Countries countries={filteredCountries} />}
     </>
   );
-}
+};
 
 export default App;
