2f19ff547731489248fd9eccf8347be395064fc7
Maximiliano Morales <maxteriaok@gmail.com>
2022-11-28 18:31:56 -0300

    Synch with oficial ver and eslint install and run

--- backend-notes/models/note.js
+++ backend-notes/models/note.js
@@ -1,27 +1,12 @@
 const mongoose = require('mongoose')
 
-// Connect to MongoDB
-const url = process.env.MONGODB_URI
-mongoose.connect(url)
-  .then(result => {
-    console.log('Connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('Error connecting to MongoDB:', error.message)
-  })
-
-
-// Schema
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
-  date: { 
-    type: Date,
-    required: true
-  },
+  date: Date,
   important: Boolean
 })
 
@@ -33,5 +18,4 @@ noteSchema.set('toJSON', {
   }
 })
 
-// Model
 module.exports = mongoose.model('Note', noteSchema)
