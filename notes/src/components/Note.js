3ff718395b8a03fa625e2184f2d538db8fc39e6c
Maximiliano Morales <maxteriaok@gmail.com>
2022-12-08 15:10:17 -0300

    update to part 5C

--- notes/src/components/Note.js
+++ notes/src/components/Note.js
@@ -1,16 +1,15 @@
-import React from "react";
+import React from 'react'
+
 const Note = ({ note, toggleImportance }) => {
-  const label = note.important ? "make not important" : "make important";
+  const label = note.important
+    ? 'make not important' : 'make important'
 
   return (
-    <>
-      <li>
-        {note.content}
-        <button onClick={toggleImportance}>{label}</button>
-        <pre>{JSON.stringify(note, null, 2)}</pre>
-      </li>
-    </>
-  );
-};
+    <li className="note">
+      {note.content}
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  )
+}
 
-export default Note;
+export default Note
